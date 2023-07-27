ea73cc1b36865944f61f896015647fd7f77cc7a1
abhay5a <74088612+abhay5a@users.noreply.github.com>
2023-07-27 22:07:16 +0530

    Update

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -8,7 +8,6 @@ const Timeline = () => {
         {data.projects.map((item, index) => (
           <TimelineItem
             heading={item.title}
-            text={item.date}
             index={index}
             key={item.title}
           />
